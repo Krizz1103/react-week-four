cb4d41865a2e7e82c91512ea247488afafce82b0
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-04-03 19:16:19 +0100

    Added loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather.js";
+import { Audio } from "react-loader-spinner";
 
 function App() {
   return (
@@ -7,6 +8,15 @@ function App() {
       <header className="App-header">
         <h1>Hello React !!!</h1>
         <Weather city="Madrid" />
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
       </header>
     </div>
   );
